bcbda18b5f1b1dae5aafb91178fef8768ee30262
Aurpan Dash <33072535+Aurpan@users.noreply.github.com>
2024-08-23 18:42:25 +0600

    feat: add basic components

---
+++ clientApp/src/App.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const App: React.FC = (props: Props) => {
+  return <div>App</div>;
+};
+
+export default App;
---
+++ clientApp/src/Components/Card/Card.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Card: React.FC = (props: Props) => {
+  return <div>Card</div>;
+};
+
+export default Card;
